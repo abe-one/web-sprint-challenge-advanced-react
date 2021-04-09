8d44311590f2d38d124b5a7ce1f37d9ae41f00b6
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-09 10:34:13 -0500

    PlantList: pass axios to state

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      plants: [],
+    };
+  }
   // add state with a property called "plants" - initialize as an empty array
 
   // when the component mounts:
@@ -10,10 +17,10 @@ export default class PlantList extends Component {
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   componentDidMount() {
-    axios.get('http://localhost:3333/plants')
-      .then(res => {
-        console.log(res);
-      });
+    axios
+      .get("http://localhost:3333/plants")
+      .then((res) => this.setState({ plants: res.data }))
+      .catch((err) => console.log(err.error[0]));
   }
 
   render() {
