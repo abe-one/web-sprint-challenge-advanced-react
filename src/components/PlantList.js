92002d3baa176bf541f8e2fccf70d818c83219e2
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-09 17:38:40 -0500

    Capitalize Direct, begin select element

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -7,8 +7,12 @@ export default class PlantList extends Component {
 
     this.state = {
       plants: [],
+      plantsDisplayed: [],
+      filter: "",
     };
   }
+
+  handleChange = (e) => {};
   // add state with a property called "plants" - initialize as an empty array
 
   // when the component mounts:
@@ -19,45 +23,60 @@ export default class PlantList extends Component {
   componentDidMount() {
     axios
       .get("http://localhost:3333/plants")
-      .then((res) => this.setState({ plants: res.data }))
+      .then((res) => {
+        this.setState({ plants: res.data });
+        this.setState({ plantsDisplayed: res.data });
+      })
       .catch((err) => console.log(err.error[0]));
   }
 
   render() {
     return (
-      <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
-          <div className="plant-card" key={plant.id} data-testid="plant-card">
-            <img className="plant-image" src={plant.img} alt={plant.name} />
-            <div className="plant-details">
-              <h2 className="plant-name">{plant.name}</h2>
-              <p className="plant-scientific-name">{plant.scientificName}</p>
-              <p>{plant.description}</p>
-              <div className="plant-bottom-row">
-                <p>${plant.price}</p>
-                <p>
-                  <span role="img" aria-label="a sun emoji">
-                    ☀️
-                  </span>{" "}
-                  {plant.light}
-                </p>
-                <p>
-                  <span role="img" aria-label="a splashing water emoji">
-                    💦
-                  </span>{" "}
-                  {plant.watering}x/month
-                </p>
+      <>
+        <select
+          name="filter"
+          value={this.state.filter}
+          onChange={this.handleChange}
+        >
+          <option name="direct" value="direct">
+            Direct
+          </option>
+        </select>
+        <main className="plant-list">
+          {this.state?.plantsDisplayed?.map((plant) => (
+            //Why this.state?.plants? the question marks
+            <div className="plant-card" key={plant.id} data-testid="plant-card">
+              <img className="plant-image" src={plant.img} alt={plant.name} />
+              <div className="plant-details">
+                <h2 className="plant-name">{plant.name}</h2>
+                <p className="plant-scientific-name">{plant.scientificName}</p>
+                <p>{plant.description}</p>
+                <div className="plant-bottom-row">
+                  <p>${plant.price}</p>
+                  <p>
+                    <span role="img" aria-label="a sun emoji">
+                      ☀️
+                    </span>
+                    {plant.light.trim().replace(/^\w/, (l) => l.toUpperCase())}
+                  </p>
+                  <p>
+                    <span role="img" aria-label="a splashing water emoji">
+                      💦
+                    </span>
+                    {plant.watering}x/month
+                  </p>
+                </div>
+                <button
+                  className="plant-button"
+                  onClick={() => this.props.addToCart(plant)}
+                >
+                  Add to cart
+                </button>
               </div>
-              <button
-                className="plant-button"
-                onClick={() => this.props.addToCart(plant)}
-              >
-                Add to cart
-              </button>
             </div>
-          </div>
-        ))}
-      </main>
+          ))}
+        </main>
+      </>
     );
   }
 }
